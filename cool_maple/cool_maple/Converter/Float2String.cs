45f03b0427fb11d5f10ad8d736a7a4ca4f36ef35
SonGwangho <rhkdfh2486@gmail.com>
2024-04-28 21:26:39 +0900

    [feat] converter 추가

---
+++ cool_maple/cool_maple/Converter/Float2String.cs
@@ -0,0 +1,18 @@
+﻿using System.Globalization;
+using System.Windows.Data;
+
+namespace cool_maple.Converter
+{
+    public class Float2String : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return (MathF.Floor((float)value * 100f)/100).ToString();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ cool_maple/cool_maple/Converter/Int2String.cs
@@ -0,0 +1,18 @@
+﻿using System.Globalization;
+using System.Windows.Data;
+
+namespace cool_maple.Converter
+{
+    public class Int2String : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value is null ? "0" : value.ToString();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
